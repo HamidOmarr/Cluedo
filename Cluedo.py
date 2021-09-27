40fe7eac151a590179e51c84323034fc3e8af7b1
HamidOmarr <99067332@mydavinci.nl>
2021-09-27 14:08:35 +0200

    next

---
+++ Cluedo.py
@@ -0,0 +1,27 @@
+print ('Welkom bij het sollicitatie van Cluedo')
+
+Geslacht = input ('Ben je een Man of Vrouw?')
+Leeftijd = input ('Hoe oud ben je?')
+Lengte = input ('Hoe lang ben je?')
+Gewicht = input ('Wat is je gewicht?')
+print ('Tactisch,Knap,Intiem,Charmant en Vanzelfsprekend') 
+Persoonlijkheid = input ('Welke van de bovenste woorden passen het meest bij je?')
+
+
+
+
+
+
+if (Geslacht == 'Man') and (Leeftijd >= 18) and (Lengte >=180) and (Persoonlijkheid == 'Tactisch'): 
+    print ('Je mag op auditie voor de rol van Kolonel van Geelen')
+
+
+
+
+
+print ('Je mag op auditie voor de rol van Mevrouw de Wit')
+print ('Je mag op auditie voor de rol van Dominee Groenewoud')
+print ('Je mag op auditie voor de rol van Rosa Roodhart')
+print ('Je mag op auditie voor de rol van Professor Pimpel')
+print ('Je mag op auditie voor de rol van Mevrouw Blaauw van Draet')
+print ('Je mag helaas niet op auditie.')
